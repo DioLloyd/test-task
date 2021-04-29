0f64dc2aef2b3f28f6ca7db2e90f6020750a8f9a
Karl <dio.lloyd@gmail.com>
2021-04-29 18:38:43 +0300

    Task Java SQL

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/staff";
+        String username = "root";
+        String password = "root";
+
+        try (Connection connection = DriverManager.getConnection(url, username, password);
+             Statement statement = connection.createStatement()) {
+            String sqlRequest = "SELECT DEPARTMENT, SUM(SALARY) AS SALARY FROM EMPLOYEES GROUP BY DEPARTMENT";
+            ResultSet resultSet = statement.executeQuery(sqlRequest);
+            while (resultSet.next()) {
+                String department = resultSet.getString("DEPARTMENT");
+                int sum = resultSet.getInt("SALARY");
+                System.out.println(department + ": " + sum);
+            }
+        }
     }
 }
